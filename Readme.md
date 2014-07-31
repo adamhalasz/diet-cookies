ec14f3355497f31ad5e7d26addb4a357824e4697
Adam Halasz <adamfsh@gmail.com>
2014-07-31 07:06:30 +0200

    v0.1.0

--- Readme.md
+++ Readme.md
@@ -1,43 +1,85 @@
-# Diet-Cookies 
-HTTP Cookie Manager for `dietjs`
+# **diet-cookies**
+An HTTP cookie manager for diet.
 
-### Install 
-If you have `dietjs` it's already installed.
+## **Install**
 ```
-	npm install diet-cookies
+npm install diet
 ```
 
-### Example usage
-```javascript
-app.get.simple('/', function(request, response){
-	// GET cookie
-	console.log(request.cookies.id);
-	
-	// SET cookie
-	response.cookies.set('name', 'john doe', { httpOnly: true }); // options are optional
-	
-	// DELETE cookie
-	response.cookies.delete('location');
-	
-	// END response
-	response.end('diet is healthy!');
+## **Example Usage**
+```js
+require('diet');
+app = new App();
+app.plugin('diet-cookies', { alias: 'cookies' });
+app.start('http://localhost:8000/');
+
+app.get('/', function($){
+    // set cookies
+    $.cookies.set('id', '1000');
+    
+    // read cookies
+    console.log($.cookies.id);  // -> '1000'    
+    console.log($.cookies)      // -> { id: '1000' }
+    
+    // delete cookies
+    $.cookies.delete('id');
+    
+    $.end();
 });
 ```
 
-### How it works?
-- You can`GET` cookies from`request.cookies[id]`  
-- You can `SET` or `UPDATE` cookies with `response.cookies.set(id, value, options)`
-- You can `DELETE` cookies with`response.cookies.delete(id)`  
-
-### Options for `response.cookies.set`
-```javascript
-{
-	"httpOnly": false, 	// true or false
-	"secure": false, 	// true or false
-	"domain": null, 	// null or domain string
-	"path": null,  		// null or path string
-}
+## **Read Cookies**
+Every cookie is stored in the `$.cookies` object.
+```js
+$.cookies.your_cookie_name
+$.cookies.other_cookie
 ```
 
-### Dependencies
-- `dietjs`
+## **Set Cookies**
+`name` and `value` is required, `options` are not.
+```js
+// api
+$.cookies.set(name, value, options);
+```
+```js
+// example with defaults
+$.cookies.set('id', '100', {
+    expire: [0,0,0],    // [days, minutes, seconds]
+    httpOnly: false,    // for http only managable cookies
+    secure: false,      // for secure cookies
+    path: '/',          // set cookies spcific to paths  
+    domain: ''          // set cookies specific to domains
+})
+```
+
+## **Delete Cookies**
+```js
+// api
+$.cookies.delete(name);
+```
+```js
+// example
+$.cookies.delete('id');
+```
+
+# MIT Licensed
+
+Copyright (c) 2014 Halász Ádám <mail@adamhalasz.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-// Cookie Handler Module
-module.exports = function(request, response){
-	response.cookieList = [];
-	response.cookieIndexes = {};
-	var deleteCookies = [];
-	var cookies = {};
-	if(isset(request.headers.cookie)){
-		var pairs = request.headers.cookie.split(';');
-		for(var i = 0; i < pairs.length; i++){
-			var pair = pairs[i].split('=');
-			var name = pair[0];
-			if(name.charAt(0) == ' '){ name = name.substring(1) }
-			cookies[name] = pair[1];
-			response.cookieIndexes[name] = i;
+function Cookies($){
+	this.__proto__.header = [];
+	this.__proto__.signal = $;
+	
+	var cookieHeader = $.header('cookie');
+	if(cookieHeader){
+		var split = cookieHeader.split(';');
+		var split_length = split.length;
+		for(var i = 0; i < split_length; i++){
+			var item = split[i];
+			var cookieSplit = item.split('=');
+			var name = cookieSplit[0].trim();
+			var value = cookieSplit[1].trim();
+			this[name] = value;
 		}
 	}
-	var actions = {
-		// options: time, httpOnly, secure, path, domain
-		set : function(name, value, options){
-			
-			// Calculate Expiration Time
-			if(isset(options.time)){
-				var days 	 = options.time[0]*1000*60*60*24;
-				var hours 	 = options.time[1]*1000*60*60;
-				var minutes  = options.time[2]*1000*60;
-				var future 	 = days+hours+minutes;
-				var now 	 = new Date().getTime();
-				var expires  = ' Expires='+ new Date(now+future).toGMTString()+' ';
-			// session cookie
-			} else {
-				var expires = '';
-			}
-			
-			var domain 	 = (!isset(options.domain))  ? '' 			 : ' Domain='+options.domain+'; ';
-			var secure 	 = (isset(options.secure)) 	 ? '; secure ' 	 : '';
-			var httpOnly = (isset(options.httpOnly)) ? '; httpOnly ' : '';
-			var path 	 = (!isset(options.path)) 	 ? '/' 			 : options.path;
-			
-			response.cookieList.push(
-				name+'='+value
-				+'; Path='+path+';'
-				+ domain
-				+ expires 
-				+ secure 
-				+ httpOnly
-			);
-			
-			response.cookieIndexes[name] = response.cookieList.length;
-			response.setHeader('Set-Cookie', response.cookieList);
-			request.cookies[name] = value;
-			
-			// for testing
-			// console.log('SET cookie WITH name `' + name + '` TO `' + value + '`', request.url.pathname);
-			
-			// Save to Cookies
-			cookies[name] = value;
-		},
-		delete: function(name, options){
-			var options = (isset(options)) ? options : {} ;
-			var domain 	= (!isset(options.domain))	? '' : ' Domain='+options.domain+'; ' ;
-			var path 	= (!isset(options.path)) ? '/' : options.path;
-			
-			delete cookies[name];
-			response.cookieList.push(name+'=;'+domain+' expires=Thu, 10 Mar 1994 01:00:00 UTC; path='+path);
-			response.setHeader('Set-Cookie', response.cookieList);
-		}
+		
+	return this;
+}
+
+Cookies.prototype.set = function(name, value, options){
+	// Safe Options Reference
+	var options = options || {};
+	
+	// Calculate Expiration Time
+	if(isset(options.expire)){
+		var days 	 = options.expire[0]*1000*60*60*24;
+		var hours 	 = options.expire[1] ? options.expire[1]*1000*60*60 : 0 ;
+		var minutes  = options.expire[2] ? options.expire[2]*1000*60 : 0;
+		var future 	 = days+hours+minutes;
+		var now 	 = new Date().getTime();
+		var expires  = ' Expires='+ new Date(now+future).toGMTString()+' ';
+	} else {
+		var expires = '';
 	}
 	
-	return [cookies, actions];
+	// Create Safe References to Cookie Options
+	var domain 	 = (!isset(options.domain))  ? '' 			 : ' Domain='+options.domain+'; ';
+	var secure 	 = (isset(options.secure)) 	 ? '; secure ' 	 : '';
+	var httpOnly = (isset(options.httpOnly)) ? '; httpOnly ' : '';
+	var path 	 = (!isset(options.path)) 	 ? '/' 			 : options.path;
+	
+	var header_string = name+'='+value
+	+'; Path='+path+';'
+	+ domain
+	+ expires 
+	+ secure 
+	+ httpOnly;
+	
+	this[name] = value;
+	this.header.push(header_string);
+	this.signal.header('set-cookie', this.header);
+};
+
+Cookies.prototype.delete = function(name, options){
+	var options = (isset(options)) ? options : {} ;
+	var domain 	= (!isset(options.domain))	? '' : ' Domain='+options.domain+'; ' ;
+	var path 	= (!isset(options.path)) ? '/' : options.path;
+
+	delete this[name];
+	this.header.push(name+'=;'+domain+' expires=Thu, 10 Mar 1994 01:00:00 UTC; path='+path);
+	this.signal.header('set-cookie', this.header);
+};
+
+// Cookie Handler Module
+module.exports.global = function($){
+	$.return(new Cookies($));
 }
